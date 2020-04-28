4b12aac0fc1d3959ce0ed2d87591916b99997d25
dalex <delphindidier0@gmail.com>
2020-04-28 15:25:02 -0400

    git pull and updated to master to check map visibility status

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 //mapbox
 
 import React, { useState } from 'react';
-import ReactMapGL, { Marker } from "react-map-gl";
+import ReactMapGL, {marker} from "react-map-gl";
 import './App.css';
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 import Main from './components/main/Main';
-
 import './App.css';
+require('dotenv').config()
+
+
+
 
 export default function App() {
   const [viewport, setViewport] = useState({
@@ -15,9 +18,12 @@ export default function App() {
     longitude: -75.1635262,
     zoom: 10,
     width: '100vw',
-    height: "100vh"
+    height: "100vh",
+    
   })
-
+      
+console.log(process.env.REACT_APP_BILLBOARD);
+console.log(process.env)
   return (
     <div>
       <Header></Header>
@@ -26,7 +32,7 @@ export default function App() {
 
       <ReactMapGL
         {...viewport}
-        mapboxApiAccessToken={process.env.REACT_APP_BUILDBOARD}
+        mapboxApiAccessToken={ process.env.REACT_APP_BUILDBOARD}
         mapStyle="mapbox://styles/ianclark226/ck9biyq6q02qp1jmg9t1ao42t"
         onViewportChange={viewport => {
           setViewport(viewport);
